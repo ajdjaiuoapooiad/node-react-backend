591e610af6928e30cbd17a0bb095648904ed0439
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-18 18:42:30 +0900

    ok

---
+++ controllers/authControllers.js
@@ -0,0 +1,12 @@
+
+
+class authControllers{
+    admin_login = async(req,res) => {
+        console.log(res.body);
+        
+    }
+}
+
+
+
+module.exports = new authControllers();
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 require('dotenv').config()
 
 
+app.use('/api',require('./routes/authRoutes'))
+
+app.get('/', (req,res) =>  res.send('My backend'))
 const port = process.env.PORT
 app.listen(port, () => console.log(`Server is running on port : ${port}`)
 );
